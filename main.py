2aa641e0a118b898d9ede491a63831efa7d544bf
Aman Kansal <kansalaman625@gmail.com>
2026-06-24 12:35:35 +0530

    feat: add prompt template with system role

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 import os 
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
+from langchain_core.prompts import ChatPromptTemplate
 
 load_dotenv()
 llm_key = os.getenv("GROQ_API")
 
-chat_bot = ChatGroq(model="llama-3.1-8b-instant",temperature=0.7,api_key=llm_key)
-print("Chatbot are active pls type exit for exit...")
-while True :
-    try :
-        user_input = input("Please enter your query...")
-        if user_input.lower() == "exit" :
-            print("Bye..have a great day")
+prompt = ChatPromptTemplate.from_messages([
+    ("system", "You are a helpful assistant who explains things in a simple way."),
+    ("human", "{user_input}")
+])
+
+chat_bot = ChatGroq(model="llama-3.1-8b-instant", temperature=0.7, api_key=llm_key)
+print("Chatbot is active. Please type exit to quit...")
+
+while True:
+    try:
+        user_input = input("Please enter your query: ")
+        
+        if user_input.lower() == "exit":
+            print("Bye.. have a great day")
             break
-        response = chat_bot.invoke(user_input)
-        print(response.content)
-    except Exception as e :
-        print(f"We are encountred with an error {e}")
+        formatted_prompt = prompt.format_messages(user_input=user_input)
+        response = chat_bot.invoke(formatted_prompt)
+        print(f"AI: {response.content}\n")
+        
+    except Exception as e:
+        print(f"We encountered an error: {e}")
